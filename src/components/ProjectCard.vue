0f0cc3aa7db0b0efaddb5c774be146058c21bd79
Antonino <drakon9927@libero.it>
2024-07-21 11:28:43 +0200

    m5

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="project-card">
+      <h3>{{ project.title }}</h3>
+      <p>{{ project.description }}</p>
+      <span>{{ project.start_date }} - {{ project.end_date }}</span>
+      <span>Status: {{ project.status }}</span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['project']
+  }
+  </script>
+  
+  <style scoped>
+    .project-card {
+      border: 1px solid #ccc;
+      padding: 20px;
+      margin-bottom: 10px;
+      border-radius: 8px;
+    }
+  </style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          {{ project.title }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted, ref } from 'vue';
+  import axios from 'axios';
+  
+  const projects = ref([]);
+  
+  onMounted(() => {
+    axios.get('http://localhost:8000/api/projects')
+      .then(response => {
+        projects.value = response.data;
+        console.log(projects.value); // Verifica in console
+      })
+      .catch(error => console.error('Error fetching projects:', error));
+  });
+  </script>
+  
+  <style scoped>
+ 
+  </style>
